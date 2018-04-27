b7e30de3bde1f469b5dd4836b8b3017a5cc2c533
Ihoey <mail@ihoey.com>
2018-04-27 10:04:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 config = {
     SITE_URL: process.env.SITE_URL || 'https://blog.ihoey.com',
-    SITE_NAME: process.env.SITE_NAME || '梦魇小栈',
+    SITE_NAME: '梦魇小栈' || process.env.SITE_NAME,
     SMTP_HOST: process.env.SMTP_HOST || 'smtp.qq.com',
     SMTP_USER: process.env.SMTP_USER || 'comment@ihoey.com',
     SMTP_PORT: process.env.SMTP_PORT || 465,
     SMTP_PASS: process.env.SMTP_PASS || 'zeuittjwgrwrbeaa',
     SENDER_EMAIL: process.env.SENDER_EMAIL || 'comment@ihoey.com',
-    SENDER_NAME: process.env.SENDER_NAME || '梦魇小栈👻'
+    SENDER_NAME: '梦魇小栈👻' || process.env.SENDER_NAME
 }
 
 module.exports = config;
